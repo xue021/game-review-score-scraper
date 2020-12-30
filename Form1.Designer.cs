8b246b35edbbff28f80656c486c5efd26e2c7044
xue021 <rhryne21@gmail.com>
2020-12-30 16:21:05 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+namespace GameReviewScoreScraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnFetch = new System.Windows.Forms.Button();
+            this.txtMainTxt = new System.Windows.Forms.RichTextBox();
+            this.numericYear = new System.Windows.Forms.NumericUpDown();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.lblNumResults = new System.Windows.Forms.Label();
+            this.numericNumResults = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblGuide = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericYear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericNumResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFetch
+            // 
+            this.btnFetch.Location = new System.Drawing.Point(12, 86);
+            this.btnFetch.Name = "btnFetch";
+            this.btnFetch.Size = new System.Drawing.Size(85, 23);
+            this.btnFetch.TabIndex = 0;
+            this.btnFetch.Text = "Fetch";
+            this.btnFetch.UseVisualStyleBackColor = true;
+            this.btnFetch.Click += new System.EventHandler(this.btnFetch_Click);
+            // 
+            // txtMainTxt
+            // 
+            this.txtMainTxt.Location = new System.Drawing.Point(12, 144);
+            this.txtMainTxt.Name = "txtMainTxt";
+            this.txtMainTxt.ReadOnly = true;
+            this.txtMainTxt.Size = new System.Drawing.Size(371, 393);
+            this.txtMainTxt.TabIndex = 2;
+            this.txtMainTxt.Text = "";
+            // 
+            // numericYear
+            // 
+            this.numericYear.Location = new System.Drawing.Point(50, 38);
+            this.numericYear.Maximum = new decimal(new int[] {
+            2100,
+            0,
+            0,
+            0});
+            this.numericYear.Minimum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.numericYear.Name = "numericYear";
+            this.numericYear.Size = new System.Drawing.Size(47, 20);
+            this.numericYear.TabIndex = 4;
+            this.numericYear.Value = new decimal(new int[] {
+            2020,
+            0,
+            0,
+            0});
+            this.numericYear.ValueChanged += new System.EventHandler(this.numericYear_ValueChanged);
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(12, 40);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(32, 13);
+            this.lblYear.TabIndex = 5;
+            this.lblYear.Text = "Year:";
+            // 
+            // lblNumResults
+            // 
+            this.lblNumResults.AutoSize = true;
+            this.lblNumResults.Location = new System.Drawing.Point(12, 60);
+            this.lblNumResults.Name = "lblNumResults";
+            this.lblNumResults.Size = new System.Drawing.Size(55, 13);
+            this.lblNumResults.TabIndex = 6;
+            this.lblNumResults.Text = "# Results:";
+            // 
+            // numericNumResults
+            // 
+            this.numericNumResults.Location = new System.Drawing.Point(63, 60);
+            this.numericNumResults.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericNumResults.Name = "numericNumResults";
+            this.numericNumResults.Size = new System.Drawing.Size(34, 20);
+            this.numericNumResults.TabIndex = 7;
+            this.numericNumResults.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.numericNumResults.ValueChanged += new System.EventHandler(this.numericNumResults_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(366, 456);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 9;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 115);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(371, 23);
+            this.progressBar.TabIndex = 17;
+            this.progressBar.Visible = false;
+            // 
+            // lblGuide
+            // 
+            this.lblGuide.AutoSize = true;
+            this.lblGuide.Location = new System.Drawing.Point(12, 9);
+            this.lblGuide.Name = "lblGuide";
+            this.lblGuide.Size = new System.Drawing.Size(273, 13);
+            this.lblGuide.TabIndex = 18;
+            this.lblGuide.Text = "Scrape the web for game review scores for a given year.\r\n";
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(395, 549);
+            this.Controls.Add(this.lblGuide);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericNumResults);
+            this.Controls.Add(this.lblNumResults);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.numericYear);
+            this.Controls.Add(this.txtMainTxt);
+            this.Controls.Add(this.btnFetch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Game Score Scraper";
+            
+            ((System.ComponentModel.ISupportInitialize)(this.numericYear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericNumResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFetch;
+        private System.Windows.Forms.RichTextBox txtMainTxt;
+        private System.Windows.Forms.NumericUpDown numericYear;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Label lblNumResults;
+        private System.Windows.Forms.NumericUpDown numericNumResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblGuide;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GameReviewScoreScraper
+{
+    public partial class Form1 : Form
+    {
+        string selectedYear = "2020";
+        int numResults = 25;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnFetch_Click(object sender, EventArgs e)
+        {
+            progressBar.Visible = true;
+
+            //get currentYear, if selected year is greater than the current year, replace selected year with current year (so you cant pull querys for years that havent happened yet
+            DateTime now = DateTime.Today;
+            int currentYear = Int16.Parse(now.ToString("yyyy"));
+            if (Int16.Parse(selectedYear) > currentYear)
+            {
+                numericYear.Value = currentYear;
+                selectedYear = currentYear.ToString();
+            }
+
+            //empty txtMainTxt
+            txtMainTxt.Text = "";
+
+            //get web data for google query of "best games <year>"
+            string webText = getWebData("https://www.google.com/search?q=best+games+" + selectedYear);
+
+
+
+            ArrayList gameList = new ArrayList();//create list of game names
+
+            //parse google query for numResults # of top games found in the google query
+            for (int i = 0; i < numResults; i++)
+            {
+                webText = webText.Substring(webText.IndexOf("amp;q=") + 6);
+                gameList.Add(webText.Substring(0, webText.IndexOf("&amp")));
+            }
+
+
+            for (int i = 0; i < numResults; i++)
+            {
+                //few char replacements, added as found (hacky i know)
+                gameList[i] = gameList[i].ToString().Replace("+", " ");
+                gameList[i] = gameList[i].ToString().Replace("%2B", "+");
+                gameList[i] = gameList[i].ToString().Replace("%C3%A9", "é");
+                gameList[i] = gameList[i].ToString().Replace("%E2%80%93", "-");
+                gameList[i] = gameList[i].ToString().Replace("%26", "&");
+                gameList[i] = gameList[i].ToString().Replace("%C3%BC", "ü");
+                gameList[i] = gameList[i].ToString().Replace("%C5%8C", "Ō");
+
+                //txtMainTxt.Text += gameList[i] + "\n";
+
+            }
+
+
+            //creates array that will store both game names and scores
+            Game[] gamesArr = new Game[gameList.Count];
+
+            //stores name and fetches + stores the games score into games arr for each item in gameList
+            for (int i = 0; i < gameList.Count; i++)
+            {
+                try
+                {
+                    gamesArr[i] = new Game(gameList[i].ToString(), getOpenCriticScoreFromUrl(getOpenCriticUrl(gameList[i].ToString())));
+
+                }
+                catch
+                {
+                    Console.WriteLine("No score found for " + gameList[i].ToString());
+                    gamesArr[i] = new Game(gameList[i].ToString(), -1);
+
+                }
+
+                //calculate total progress in fetching data and update progress bar
+                double prog = Math.Round((double)(i + 1) / (double)gameList.Count * 100);
+                Console.WriteLine(prog + "%");
+                progressBar.Value = (int)prog;
+            }
+
+            //sort the array of games in descending order of score
+            Array.Sort(gamesArr, new GameScoreComparer());
+            Array.Reverse(gamesArr);
+
+            //write game score data to text field in form: <game name>(<score>), if a score is unavailable put "n/a" instead of -1
+            for (int i = 0; i < gamesArr.Length; i++)
+            {
+                if (gamesArr[i].score > -1)
+                {
+                    Console.WriteLine(gamesArr[i].name + " - " + gamesArr[i].score);
+                    txtMainTxt.Text += gamesArr[i].name + " (" + gamesArr[i].score + ")\n";
+                }
+                else
+                {
+                    Console.WriteLine(gamesArr[i].name + " - " + gamesArr[i].score);
+                    txtMainTxt.Text += gamesArr[i].name + " (n/a)\n";
+                }
+
+            }
+        }
+
+
+        //this function takes a url and returns the raw html text of the page
+        private string getWebData(string siteUrl)
+        {
+            //this code snippet modified from: https://stackoverflow.com/questions/16642196/get-html-code-from-website-in-c-sharp
+            string webText = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(siteUrl);
+            request.Timeout = 2000; //set timeout is important 
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+                if (String.IsNullOrWhiteSpace(response.CharacterSet))
+                    readStream = new StreamReader(receiveStream);
+                else
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                string data = readStream.ReadToEnd();
+                webText = data;
+                response.Close();
+                readStream.Close();
+            }
+            return webText;
+            //end snippet
+        }
+        
+        //functions to update variables when their respective fields change
+        private void numericYear_ValueChanged(object sender, EventArgs e)
+        {
+            selectedYear = numericYear.Value.ToString();
+        }
+
+        private void numericNumResults_ValueChanged(object sender, EventArgs e)
+        {
+            numResults = Int16.Parse(numericNumResults.Value.ToString());
+        }
+
+
+
+
+        //gets the url of the first page from opencritic in a google query using the games name + opencritic as the search (i.e.: "Cyberpunk 2077 opencritic")
+        private string getOpenCriticUrl(string gameName)
+        {
+
+            string query = gameName + "+opencritic";
+            query = query.Replace(" ", "+");
+            string searchResult = getWebData("https://www.google.com/search?q=" + query);
+
+
+            searchResult = searchResult.Substring(searchResult.IndexOf("https://opencritic.com/"));
+            searchResult = searchResult.Substring(0, searchResult.IndexOf("&amp"));
+            return searchResult;
+        }
+
+        //finds the score value for a game given an opencritic url for a game, if it cant returns -1
+        private short getOpenCriticScoreFromUrl(string ocUrl)
+        {
+            string webData = getWebData(ocUrl);
+            webData = webData.Substring(webData.IndexOf("ratingValue") + 14);
+            webData = webData.Substring(0, webData.IndexOf(","));
+            Console.WriteLine(webData);
+            try
+            {
+                return (short)(float.Parse(webData) + 0.5f);
+            }
+            catch
+            {
+                return -1;
+            }
+
+        }
+
+       
+    }
+}
+
+//simple object to store game name and score
+class Game
+{
+    public string name = "null";
+    public short score = -1;
+    public Game(string gameName, short gameScore)
+    {
+        name = gameName;
+        score = gameScore;
+
+    }
+}
+
+//used for comparing scores of game objects
+class GameScoreComparer : IComparer
+{
+    public int Compare(object x, object y)
+    {
+        return (new CaseInsensitiveComparer()).Compare(((Game)x).score, ((Game)y).score);
+    }
+}
